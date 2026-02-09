700f8e62b879884cc0180ba92f580c488f30b542
soknanprogramming <poch.soknan@gmail.com>
2026-02-10 06:53:25 +0700

    just add more buttom

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import react_svg from '../assets/react.svg'
 import ume_png from '../assets/ume.png'
 import { Link, useLocation } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
+    const [showMore, setShowMore] = useState<boolean>(false)
     const location = useLocation()
     console.log(location.pathname);
     return (
-        <div className='bg-amber-300 flex py-0.5 mt-1 border-solid border-y-2 border-amber-200'>
-            <div className='items-center flex mx-2'>
-                <Link to="/"><img src={react_svg} className='animate-spin' alt="react" /></Link>
-                <a href="http://"><img src={ume_png} className='size-10 animate-bounce' alt="ume" /></a>
+        <div className='bg-amber-300 flex justify-between py-0.5 mt-1 border-solid border-y-2 border-amber-200'>
+            <div className='flex ml-3'>
+                <div className='items-center flex mx-2'>
+                    <Link to="/"><img src={react_svg} className='animate-spin' alt="react" /></Link>
+                    <a href="http://"><img src={ume_png} className='size-10 animate-bounce' alt="ume" /></a>
+                </div>
+                <ul className='flex mx-4 items-center'>
+                    <Link to="/"><li className={addStyleToLink(location.pathname === "/")}>Home</li></Link>
+                    <Link to="about"><li className={addStyleToLink(location.pathname === "/about")}>About</li></Link>
+                    <Link to="add_item"><li className={addStyleToLink((location.pathname === "/add_item") || (location.pathname === "/view_item"))}>Items</li></Link>
+                </ul>
             </div>
-            <ul className='flex mx-4 items-center'>
-                <Link to="/"><li className={addStyleToLink(location.pathname === "/")}>Home</li></Link>
-                <Link to="about"><li className={addStyleToLink(location.pathname === "/about")}>About</li></Link>
-                <Link to="add_item"><li className={addStyleToLink((location.pathname === "/add_item") || (location.pathname === "/view_item"))}>Items</li></Link>
-            </ul>
+            <div className='mr-30 items-center flex h-full'>
+                <div className='h-full relative'>
+                    <button onClick={() => setShowMore(!showMore)} className='bg-amber-600 h-full px-10'>More</button>
+                    <div className={(showMore ? 'flex' : 'hidden') + ' absolute bg-blue-400 mt-0.5 w-50 left-0'}>
+                        Hello b
+                        Hello b
+                        Hello b
+                        Hello b
+                        Hello b
+                        Hello b
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
